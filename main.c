c3462d9f5a4a86a526c4a5b43cf4650349fdcd60
Victhor-Alexis <victhoralexis123@gmail.com>
2021-02-09 19:18:47 -0300

    Add aim trajectory and movement.

--- main.c
+++ main.c
@@ -6,27 +6,84 @@
 #include <assert.h>
 #include <string.h>
 #include <time.h>
+#include <math.h>
 
 char randChar();
 int getch();
+int mExist(int x, int y);
+float calculatingM(int x, int y);
+float calculatingN(int x, int y, float m);
+float lineEquationAproxValue(int x, int y, float m, float n);
 
 int main(void) {
     int i,j;
     char map[9][16];
     char piece = randChar();
     char action = ' ';
+    int xf = 8, yf = 7, x, y, auxX, auxY;
+    int mNaN = 0;
+    float m, n;
+    float mostAprox;
 
     do {
+        // map initialization
+        for (i = 8; i >= 0; --i)
+            for (j = 0; j < 16; ++j)
+                map[i][j] = ' ';
+
+        x = xf;
+        y = yf;
+
+        //printf("m:%f, n:%f\n", m,n);
+
         // Loading map
         for (i = 8; i >= 0; --i) {
             for (j = 0; j < 16; ++j) {
-                if (i == 8 || i == 0 || j == 0 || j == 15)
+                if (i == 8 || i == 0 || j == 0 || j == 15) {
                     map[i][j] = '#';
-                else if (i == 1 && j == 8)
+                }
+                else if (i == 1 && j == 8) {
                     map[i][j] = piece;
-                else
-                    map[i][j] = ' ';
+                }
+                else if (i == yf && j == xf) {// Drawing aim trajectory
+                    if (mNaN == 0) {
+                        while (y > 1) {
+                            map[y][x] = '.';
+                            --y;
+                        }
+                    }
+                    else {
+                        map[y][x] = '.';
 
+                        while(y > 2) {
+                            mostAprox = 1000000;
+                            //printf("R(%d,%d)\n", x,y);
+                            // Finding the most approximate point for the non-continuous terminal space:
+                            for (int k = 1; k > -2; --k) {
+                                for (int l = -1; l < 1; ++l) {
+                                    if (!(k == 0 && l == 0)) {
+                                        if (mostAprox > fabs(lineEquationAproxValue(x+k,y+l,m,n))) {
+                                            mostAprox = fabs(lineEquationAproxValue(x+k,y+l,m,n));
+                                            auxX = x+k;
+                                            auxY = y+l;
+                                            //printf("menor: %f\n", mostAprox);
+                                        }
+                                        //printf("%f ", lineEquationAproxValue(x+k,y+l,m,n));
+                                        //printf("(x+%d,y+%d)\n", k,l);
+                                        //printf("(%d,%d)\n", x+k,y+l);
+                                        //printf("(%d,%d)\n", x,y);
+                                    }// "adjacent points"
+                                }
+                            }// k loop brace
+
+                            //printf("C(%d,%d)\n", auxX, auxY);
+                            //printf("\n");
+                            x = auxX;
+                            y = auxY;
+                            map[auxY][auxX] = '.';
+                        }
+                    }
+                }
             }
         }
 
@@ -40,6 +97,42 @@ int main(void) {
 
         action = getch();
 
+        switch (action) {// movement
+            case 'w':
+                piece = randChar();
+            break;
+
+            case 'a':
+                if (map[yf][xf-1] == ' ') {// Respecting limits of the map
+                    --xf;
+                }
+                else if (map[yf][xf-1] != '#') {// piece in the way
+                    --xf;
+                    --yf;
+                }
+
+                m = calculatingM(xf,yf);
+                n = calculatingN(xf,yf,m);
+
+                mNaN = mExist(xf,yf);
+            break;
+
+            case 'd':
+                if (map[yf][xf+1] == ' ') {// Respecting limits of the map
+                    ++xf;
+                }
+                else if (map[yf][xf+1] != '#') {// piece in the way
+                    ++xf;
+                    --yf;
+                }
+
+                m = calculatingM(xf,yf);
+                n = calculatingN(xf,yf,m);
+
+                mNaN = mExist(xf,yf);
+            break;
+        }
+
         system("clear");
 
     } while (action != 'c');
@@ -52,6 +145,22 @@ char randChar() {
     char letra;
 
     return ('A' + rand() % 5);
+}
+
+int mExist(int x, int y) { // Checking if the angular coefficient is equal to tan(90 deg);
+    return !(x == 8 && y == 7);
+}
+
+float calculatingM(int x, int y) {
+    return ((float)(y - 1)/(float)(x - 8));
+}
+
+float calculatingN(int x, int y, float m) {
+    return (float)(y - m*x);
+}
+
+float lineEquationAproxValue(int x, int y, float m, float n) {
+    return (float)(m*x + n - y);
 }
 
 int getch() {
